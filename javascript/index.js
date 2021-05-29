614f16b00892c9a651311864d85038a26bb1761e
Gabriel Oliveira <devyeetz@gmail.com>
2021-05-29 10:24:58 -0300

    dev

---
+++ javascript/index.js
@@ -0,0 +1,23 @@
+const {getSize} = require('import-jb/dist/src/packageInfo');
+
+class ImportCostPlugin {
+    onMessage(p, messageWriter) {
+        const {seq, arguments: packageInfo} = JSON.parse(p);
+        const config = {maxCallTime: Infinity, concurrent: true};
+        getSize(packageInfo, config).then((pkg) => {
+            messageWriter.write(JSON.stringify({request_seq: seq, package: pkg}))
+        }).catch((e) => {
+            messageWriter.write(JSON.stringify({request_seq: seq, error: e}))
+        })
+    }
+}
+
+class ImportCostPluginFactory {
+    create() {
+        return {languagePlugin: new ImportCostPlugin()}
+    }
+}
+
+module.exports = {
+    factory: new ImportCostPluginFactory()
+};
---
+++ src/main/kotlin/com/github/yeetzy/importjb/ServiceProtocol.kt
@@ -0,0 +1,20 @@
+package com.github.yeetzy.importjb
+
+import com.intellij.lang.javascript.service.JSLanguageServiceUtil
+import com.intellij.lang.javascript.service.protocol.JSLanguageServiceInitialState
+import com.intellij.lang.javascript.service.protocol.JSLanguageServiceNodeStdProtocolBase
+import com.intellij.lang.javascript.service.protocol.LocalFilePath
+import com.intellij.openapi.project.Project
+import com.intellij.util.Consumer
+
+class ServiceProtocol(project: Project, readyConsumer: Consumer<*>) : JSLanguageServiceNodeStdProtocolBase(project, readyConsumer) {
+    override fun createState(): JSLanguageServiceInitialState {
+        val result = JSLanguageServiceInitialState()
+        result.pluginName = "import-jb"
+        val file = JSLanguageServiceUtil.getPluginDirectory(this.javaClass, "lib/index.js")
+        result.pluginPath = LocalFilePath.create(file.absolutePath)
+        return result
+    }
+
+    override fun dispose() {}
+}
